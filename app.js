b99dbfaf4c644136b1fe33d0fe550ee4ddffcde5
Hazem Adel <hzmtheshadow@gmail.com>
2021-07-11 03:52:14 +0200

    implemented firebase in app.js and database create on submit

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
+var firebase = require("firebase/app");
+
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+
 const express = require('express');
 
 const bodyParser = require("body-parser");
 
 
+firebase.initializeApp({
+    apiKey: "AIzaSyAbNoY1KoqC3VknnGzAjHFB-eJYnCmx_hg",
+    authDomain: "cvexpress-68b7a.firebaseapp.com",
+    projectId: "cvexpress-68b7a",
+    storageBucket: "cvexpress-68b7a.appspot.com",
+    messagingSenderId: "461719487106",
+    appId: "1:461719487106:web:8b33e9b914a84a99252a9b",
+    measurementId: "G-7SVH1N0GQZ"
+});
+const db = firebase.firestore();
+
+
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-
-
+// const usersRef = ref.child('users');
+// usersRef.set({
+//   alanisawesome: {
+//     date_of_birth: 'June 23, 1912',
+//     full_name: 'Alan Turing'
+//   },
+//   gracehop: {
+//     date_of_birth: 'December 9, 1906',
+//     full_name: 'Grace Hopper'
+//   }
+// });
 // Home Page
 
 app.get("/", (req,res) =>{
@@ -23,16 +51,46 @@ app.get("/form", (req, res) => {
 app.post("/form", function(req,res){
   var fName = req.body.fName;
   var lName =req.body.lName;
-  var adress = req.body.adress;
-  var adress2 = req.body.adress2;
+  var address = req.body.adress;
+  var address2 = req.body.adress2;
   var linkedIn = req.body.linkedIn;
   var milotaryStatus = req.body.militaryStatus;
   var position =req.body.position;
   var degree = req .body.degree;
   var major = req.body.major;
   var experience = req.body.experience;
+  var email = req.body.email;
+  var skills = req.body.skills;
+  var languages = req.body.languages;
 
-  console.log(fName,lName)
+//Firebase js code
+
+// submit_cv.addEventListener("click", function () {
+
+    db.collection("cvs").add({
+        firstName: fName,
+        lastName: lName,
+        email: email,
+        linkedIn: linkedIn,
+        address: address,
+        address2: address2,
+        job_title: position,
+        Major: major,
+        experience: experience,
+        Skills: skills,
+        Languages: languages,
+    }).then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+
+    // console.log(value)
+// });
+
+//firebase js end code
+  console.log(fName,lName,linkedIn)
   res.send("Your Details have been successfully uploaded")
 });
 
